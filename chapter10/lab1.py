6af9ac03a59f5202706404a074110479cd7d6fde
Stephen Gillespie-Jones <sjgillespiejones@gmail.com>
2024-07-22 14:30:06 +1000

    main - did chapter 10 lab1

---
+++ chapter10/lab1.py
@@ -0,0 +1,110 @@
+import numpy as np, pandas as pd
+from matplotlib.pyplot import subplots
+from sklearn.linear_model import (LinearRegression, LogisticRegression, Lasso)
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from ISLP import load_data
+from ISLP.models import ModelSpec as MS
+from sklearn.model_selection import (train_test_split, GridSearchCV)
+
+import torch
+from torch import nn
+from torch.optim import RMSprop
+from torch.utils.data import TensorDataset
+
+from torchmetrics import (MeanAbsoluteError , R2Score)
+from torchinfo import summary
+from torchvision.io import read_image
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import CSVLogger
+
+from pytorch_lightning import seed_everything
+seed_everything(0, workers=True)
+torch.use_deterministic_algorithms(True, warn_only=True)
+
+from torchvision.datasets import MNIST, CIFAR100
+from torchvision.models import (resnet50,ResNet50_Weights)
+from torchvision.transforms import (Resize,Normalize , CenterCrop , ToTensor)
+
+from ISLP.torch import (SimpleDataModule , SimpleModule ,ErrorTracker , rec_num_workers)
+from ISLP.torch.imdb import (load_lookup ,load_tensor , load_sparse ,load_sequential)
+
+from glob import glob
+import json
+
+Hitters = load_data('Hitters').dropna()
+n = Hitters.shape[0]
+
+model = MS(Hitters.columns.drop('Salary'), intercept=False)
+X = model.fit_transform(Hitters).to_numpy()
+Y = Hitters['Salary'].to_numpy()
+
+(X_train, X_test, Y_train, Y_test) = train_test_split(X, Y, test_size=1/3, random_state=1)
+
+hit_lm = LinearRegression().fit(X_train, Y_train)
+Yhat_test = hit_lm.predict(X_test)
+mean_error = np.abs(Yhat_test - Y_test).mean()
+print(mean_error)
+
+scaler = StandardScaler(with_mean=True, with_std=True)
+lasso = Lasso(warm_start=True, max_iter=30_000)
+standard_lasso = Pipeline(steps=[('scaler', scaler), ('lasso', lasso)])
+
+X_s = scaler.fit_transform(X_train)
+n = X_s.shape[0]
+lam_max = np.fabs(X_s.T.dot(Y_train - Y_train.mean())).max() / n
+params_grid = { 'alpha' : np.exp(np.linspace(0, np.log(0.01), 100)) * lam_max}
+
+cv = KFold(10, shuffle=True, random_state=1)
+grid = GridSearchCV(lasso, params_grid, cv=cv, scoring='neg_mean_absolute_error')
+grid.fit(X_train, Y_train)
+# print(grid.best_estimator_)
+
+trained_lasso = grid.best_estimator_
+Yhat_test = trained_lasso.predict(X_test)
+mean_error = np.fabs(Yhat_test - Y_test).mean()
+print(mean_error)
+
+
+class HittersModel(nn.Module):
+
+    def __init__(self, input_size):
+        super(HittersModel, self).__init__()
+        self.flatten = nn.Flatten()
+        self.sequential = nn.Sequential(
+            nn.Linear(input_size, 50),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            nn.Linear(50, 1))
+
+    def forward(self, x):
+        x = self.flatten(x)
+        return torch.flatten(self.sequential(x))
+
+
+hit_model = HittersModel(X.shape[1])
+# print(summary(hit_model, input_size=X_train.shape, col_names=['input_size', 'output_size', 'num_params']))
+
+X_train_t = torch.tensor(X_train.astype(np.float32))
+Y_train_t = torch.tensor(Y_train.astype(np.float32))
+hit_train = TensorDataset(X_train_t, Y_train_t)
+X_test_t = torch.tensor(X_test.astype(np.float32))
+Y_test_t = torch.tensor(Y_test.astype(np.float32))
+hit_test = TensorDataset(X_test_t, Y_test_t)
+
+max_num_workers = rec_num_workers()
+
+hit_dm = SimpleDataModule(hit_train, hit_test, batch_size=32, num_workers=min(4, max_num_workers), validation=hit_test)
+hit_module = SimpleModule.regression(hit_model, metrics={'mae': MeanAbsoluteError()})
+hit_logger = CSVLogger('logs', name='hitters')
+
+def main():
+    hit_trainer = Trainer(deterministic=True, max_epochs=50, log_every_n_steps=5, logger=hit_logger,
+                          callbacks=[ErrorTracker()])
+    hit_trainer.fit(hit_module, datamodule=hit_dm)
+    print(hit_trainer.test(hit_module, datamodule=hit_dm))
+
+if __name__ == '__main__':
+    main()
